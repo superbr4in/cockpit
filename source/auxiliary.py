61c0e504f2f44c052717778a786f593ab36ce004
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-10-05 18:01:25 +0200

    Improved user interface.

--- source/auxiliary.py
+++ source/auxiliary.py
@@ -1,5 +1,11 @@
 import subprocess
 
+def to_unicode(string):
+    return string.decode('utf-8')
+
 def pipe_command(command):
-    with subprocess.Popen(command.split(), stdout=subprocess.PIPE) as p:
-        return p.stdout.read().decode('utf-8')
+    with subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
+        stdout = p.stdout.read()
+        if len(stdout) > 0:
+            return to_unicode(stdout)
+        return to_unicode(p.stderr.read())
--- source/cockpit_tool.cpp
+++ source/cockpit_tool.cpp
@@ -12,19 +12,28 @@ int main(int argc, char* argv[])
         return 0;
     }
 
+    std::string const command = argv[1];
+
     unsigned short n_lines, n_columns;
     terminal_get_size(&n_lines, &n_columns);
 
     // Create space not to overwrite existing output
     std::cout << std::string(n_lines, '\n') << std::flush;
 
-    // Print exit text
-    terminal_set_cursor_position(n_lines, 1);
-    std::cout << "Press ENTER to exit...";
-
     initialize_python();
 
-    cockpit cockpit(1000, 1, [argv] { return py_pipe_command(argv[1]); });
+    cockpit cockpit(1000, 1,
+        [command, &n_lines, &n_columns]
+        {
+            terminal_get_size(&n_lines, &n_columns);
+
+            // Print exit text
+            terminal_set_cursor_position(n_lines, 1);
+            terminal_clear_line();
+            std::cout << std::string("Press ENTER to exit...").substr(0, n_columns);
+
+            return py_pipe_command(command);
+        });
     cockpit.start();
 
     std::cin.get();
